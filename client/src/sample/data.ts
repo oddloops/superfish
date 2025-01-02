8b736e33046a45ba68103882d1870a8efc226b02
Vinci Wu <vinciwu.vwu@gmail.com>
2025-01-02 18:40:15 -0500

    Added placeholder data for development purposes

---
+++ client/src/sample/data.ts
@@ -0,0 +1,53 @@
+import { Notebook } from "../app/shared/models/Notebook";
+
+export const sample_notebook: Notebook[] = [
+  {
+    id: 1,
+    title: "Sample Notebook 1",
+    tag: ["sample", "tag1"],
+    imageUrl: "assets/images/star-1.jpg",
+    favorite: true,
+  },
+  {
+    id: 2,
+    title: "Sample Notebook 2",
+    tag: ["sample", "tag2"],
+    imageUrl: "assets/images/star-1.jpg",
+    favorite: false,
+  },
+  {
+    id: 3,
+    title: "Sample Notebook 3",
+    tag: ["sample", "tag3"],
+    imageUrl: "assets/images/star-1.jpg",
+    favorite: true,
+  },
+  {
+    id: 4,
+    title: "Sample Notebook 4",
+    tag: ["sample", "tag4"],
+    imageUrl: "assets/images/star-1.jpg",
+    favorite: false,
+  },
+  {
+    id: 5,
+    title: "Sample Notebook 5",
+    tag: ["sample", "tag5"],
+    imageUrl: "assets/images/star-1.jpg",
+    favorite: false,
+  },
+  {
+    id: 6,
+    title: "Sample Notebook 6",
+    tag: ["sample", "tag6"],
+    imageUrl: "assets/images/star-1.jpg",
+    favorite: false,
+  },
+  {
+    id: 7,
+    title: "Sample Notebook 7",
+    tag: ["sample", "tag7"],
+    imageUrl: "assets/images/star-1.jpg",
+    favorite: false,
+  },
+];
