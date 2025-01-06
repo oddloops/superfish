f94abc9f25fd8047740d87f0a4b8debde3feaa54
Vinci Wu <vinciwu.vwu@gmail.com>
2025-01-06 14:20:20 -0500

    Added missing RouterLink import

--- client/src/app/components/partials/header/header.component.ts
+++ client/src/app/components/partials/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SearchComponent } from "../search/search.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [SearchComponent],
+  imports: [RouterLink, SearchComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
