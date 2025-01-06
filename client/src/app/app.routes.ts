c5f2ebc339a9b65af2ea7be3ff799e83ffcc90e3
Vinci Wu <vinciwu.vwu@gmail.com>
2025-01-05 21:14:22 -0500

    Added route for notebook and a wildcard route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
+import { NotebookComponent } from './components/pages/notebook/notebook.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'search/:searchTerm', component: HomeComponent}
+  {path: 'search/:searchTerm', component: HomeComponent},
+  {path: 'notebook/:notebookId', component: NotebookComponent},
+  {path: '**', component: HomeComponent}
 ];
