72dabda23f46683844d39d67a7b2b2922f21e772
Vinci Wu <vinciwu.vwu@gmail.com>
2025-01-08 16:31:55 -0500

    Added sample data into server side

---
+++ backend/src/sample/data.ts
@@ -0,0 +1,44 @@
+export const sample_notebook: any[] = [
+  {
+    id: 1,
+    title: "Science",
+    imageUrl: "assets/images/star-1.jpg",
+    favorite: true,
+  },
+  {
+    id: 2,
+    title: "Material Science",
+    imageUrl: "assets/images/star-1.jpg",
+    favorite: false,
+  },
+  {
+    id: 3,
+    title: "Calculus",
+    imageUrl: "assets/images/star-1.jpg",
+    favorite: true,
+  },
+  {
+    id: 4,
+    title: "Statistics",
+    imageUrl: "assets/images/star-1.jpg",
+    favorite: false,
+  },
+  {
+    id: 5,
+    title: "Linear Algebra",
+    imageUrl: "assets/images/star-1.jpg",
+    favorite: false,
+  },
+  {
+    id: 6,
+    title: "Discrete",
+    imageUrl: "assets/images/star-1.jpg",
+    favorite: false,
+  },
+  {
+    id: 7,
+    title: "Physics",
+    imageUrl: "assets/images/star-1.jpg",
+    favorite: false,
+  },
+];
