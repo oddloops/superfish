cec704b682edddd904690257228d3505c4ef1acf
Vinci Wu <vinciwu.vwu@gmail.com>
2025-01-08 16:38:26 -0500

    Added @types/cors to server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,13 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
 
+// ***** Express App ***** //
 const app: Express = express();
 
+// ***** Middleware ***** //
+app.use(cors());
+
+// ***** API ***** // 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is live!");
 });
