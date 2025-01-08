098a3994dca41fe0a350daffc75f546f59fdef1b
Vinci Wu <vinciwu.vwu@gmail.com>
2025-01-08 16:32:29 -0500

    Updated index.ts to only run the server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,10 @@
-import express, { Express, Request, Response } from "express";
+import server from './server';
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Server is live!");
-});
-
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
