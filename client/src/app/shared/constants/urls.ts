114af6db71dc43d19638b318fc713de133705964
Vinci Wu <vinciwu.vwu@gmail.com>
2025-01-09 16:41:26 -0500

    Edited urls to correspond to server changes

--- client/src/app/shared/constants/urls.ts
+++ client/src/app/shared/constants/urls.ts
@@ -1,5 +1,5 @@
 const BASE_URL = 'http://localhost:5200';
 
-export const NOTEBOOKS_URL = BASE_URL + '/api/notebooks';
+export const NOTEBOOKS_URL = BASE_URL + '/notebooks';
 export const NOTEBOOKS_BY_SEARCH_URL = NOTEBOOKS_URL + '/search/';
 export const NOTEBOOKS_BY_ID_URL = NOTEBOOKS_URL + '/';
