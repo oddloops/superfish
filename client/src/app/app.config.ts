81743432f4d4a7fa9d16e162fb39177962640728
Vinci Wu <vinciwu.vwu@gmail.com>
2025-01-09 16:41:05 -0500

    Added provideHttpClient with fetch

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,8 +1,12 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withFetch())
+  ]
 };
