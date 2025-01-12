0053071b37176081f271b1617157eaeb92438981
Vinci Wu <vinciwu.vwu@gmail.com>
2025-01-12 17:32:50 -0500

    Added a route to the login component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NotebookComponent } from './components/pages/notebook/notebook.component';
+import { LoginPageComponent } from './components/pages/login-page/login-page.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'login', component: LoginPageComponent},
   {path: 'search/:searchTerm', component: HomeComponent},
   {path: 'notebook/:notebookId', component: NotebookComponent},
   {path: '**', component: HomeComponent}
