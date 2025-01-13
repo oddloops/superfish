d7ef10e9e835373debbcc2dcb19a4ef44c017f53
Vinci Wu <vinciwu.vwu@gmail.com>
2025-01-12 22:33:05 -0500

    Added new route for signup to login page component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NotebookComponent } from './components/pages/notebook/notebook.component';
-import { LoginPageComponent } from './components/pages/login-page/login-page.component';
+import { LoginPageComponent } from '@components/pages/login-page/login-page.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginPageComponent},
+  {path: 'signup', component: LoginPageComponent},
   {path: 'search/:searchTerm', component: HomeComponent},
   {path: 'notebook/:notebookId', component: NotebookComponent},
   {path: '**', component: HomeComponent}
