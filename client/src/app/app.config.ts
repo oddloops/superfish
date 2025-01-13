c35c0756d167f098cba233409d53211b812ca4d3
Vinci Wu <vinciwu.vwu@gmail.com>
2025-01-12 22:32:00 -0500

    Added toastr and its animations to the config

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,11 +2,15 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    provideToastr(),
+    provideAnimations()
   ]
 };
