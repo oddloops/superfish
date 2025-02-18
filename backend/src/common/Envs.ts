9af23294f40e3e71abc1c5406f47786225bd2ba7
Vinci Wu <vinciwu.vwu@gmail.com>
2025-02-17 21:45:40 -0500

    Added environment variables for Atlas cluster and DB name

--- backend/src/common/Envs.ts
+++ backend/src/common/Envs.ts
@@ -4,7 +4,9 @@ dotenv.config();
 
 const Envs = {
   "JWT_SECRET": process.env.JWT_SECRET as string,
-  "PORT": process.env.PORT
+  "PORT": process.env.PORT,
+  "ATLAS_URI": process.env.ATLAS_URI,
+  "DB_NAME": process.env.DB_NAME
 }
 
 export default Envs;
